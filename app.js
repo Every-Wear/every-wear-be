0ee131d94c600188ee8ea349de5d4a4e37b315f7
Nuung <qlgks1@naver.com>
2023-05-22 20:53:20 +0900

    modify: app.js update

--- app.js
+++ app.js
@@ -74,14 +74,11 @@ app.use(function (err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error pag
-    res.status(err.status || 500);
-
-    res.json({
+    // render the error page
+    return res.status(err.status || 500).json({
         message: err.message,
         error: err
     });
-
 });
 
 
