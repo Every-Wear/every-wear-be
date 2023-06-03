57c48b4408823d397553db7707a88e23a95ac840
Nuung <qlgks1@naver.com>
2023-06-03 19:06:41 +0900

    modify: ssl cert script update v2

--- nginx/data/init-letsencrypt.sh
+++ nginx/data/init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(every-wear.kro.kr www.every-wear.kro.kr)
 rsa_key_size=4096
-data_path="./nginx/data/certbot"
+data_path="./"
 email="qlgks1@naver.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
