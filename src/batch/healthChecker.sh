0cd4cea6760e3f85d6f0688439ae186f06506e55
Nuung <qlgks1@naver.com>
2023-06-10 21:20:03 +0900

    feature: server health checker v3

--- src/batch/healthChecker.sh
+++ src/batch/healthChecker.sh
@@ -3,10 +3,10 @@
 # crontab 등록, SLACK_TOKEN 환경 변수 세팅 먼저 필요
 # */5 * * * * /home/ubuntu/every-wear-be/src/batch/healthChecker.sh >> /home/ubuntu/crontab-log.log
 
-urls=("https://localhost/api/ping" "https://everywear.kro.kr/")
+urls=("http://localhost/api/ping" "https://everywear.kro.kr/")
 for url in "${urls[@]}"; do
     response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-    if [[ $response == "200" ]]; then
+    if [[ $response == "200" || $response == "301" ]]; then
         echo "$(date): $url is healthy."
     else
         echo "$(date): $url is down (HTTP response: $response)."
@@ -15,7 +15,7 @@ for url in "${urls[@]}"; do
         --header "Authorization: Bearer $SLACK_TOKEN" \
         --header "Content-Type: application/json" \
         --data '{
-            "text":"<@U034N5TBUSZ> '$url' 의 서버 상태가 정상이 아닙니다!! sudo ssh -i ~/.ssh/every-wear.key ubuntu@146.56.119.132 / fe:158.180.92.143, <'$url'|여기를 클릭>",
+            "text":"<@U034N5TBUSZ> '$url' 의 서버 상태가 정상이 아닙니다!! \n sudo ssh -i ~/.ssh/every-wear.key ubuntu@146.56.119.132 / fe:158.180.92.143, <'$url'|여기를 클릭>",
             "channel": "C034R2SQRDY"
         }'
     fi
