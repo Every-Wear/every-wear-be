fcf1e5b4703473539b9a12c7de72d7db7d39b304
Nuung <qlgks1@naver.com>
2023-06-11 23:50:53 +0900

    modify: clustering mode

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,14 +2,17 @@ module.exports = {
     apps: [
         {
             name: "everywear-be",
-            script: "yarn",
+            script: "./www/www.js",
             interpreter: "sh", // Shell 스크립트의 인터프리터 지정
+            instances: "max", // match the number of CPUs on the machine
+            exec_mode: "cluster", // Run multiple child processes
             args: "start",
             watch: true,
             watch_options: {
                 usePolling: true,
                 interval: 1000, // 변경 감지 주기를 1초로 설정
             },
+            ignore_watch: ["logs", "tmp", "public", "static"], // 감시에서 제외할 파일 또는 디렉토리 패턴
         },
     ],
 };
